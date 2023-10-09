66896f7d02bce36ca9bc9097b40399119bbc2775
okazaki-kk <kohei-okazaki@c-fo.com>
2023-10-09 19:47:55 +0900

    add isucon example

---
+++ isucon/ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "isucon" {
+  ami                         = "ami-0676c829e30e00846"
+  instance_type               = "c6i.large"
+  key_name                    = "otyamura-isucon"
+  subnet_id                   = aws_subnet.isucon_public_a.id
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [aws_security_group.isucon.id]
+
+  tags = {
+    Name = "otyamura-isucon"
+  }
+}
+
+resource "aws_instance" "isucon-bench" {
+  ami           = "ami-0582a2a7fbe79a30d"
+  instance_type = "c6i.large"
+  key_name      = "otyamura-isucon"
+  subnet_id     = aws_subnet.isucon_public_c.id
+
+  vpc_security_group_ids = [aws_security_group.isucon_bench.id]
+
+  tags = {
+    Name = "otyamura-isucon-bench"
+  }
+}
---
+++ isucon/isucon.tf
@@ -0,0 +1,181 @@
+resource "aws_vpc" "isucon" {
+  cidr_block = "172.31.0.0/16"
+
+  tags = {
+    Name = "isucon VPC"
+  }
+}
+
+resource "aws_subnet" "isucon_public_a" {
+  vpc_id                  = aws_vpc.isucon.id
+  cidr_block              = "172.31.0.0/20"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "isucon Public a"
+  }
+}
+
+resource "aws_subnet" "isucon_public_c" {
+  vpc_id                  = aws_vpc.isucon.id
+  cidr_block              = "172.31.16.0/20"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "isucon Public c"
+  }
+}
+
+resource "aws_subnet" "isucon_public_d" {
+  vpc_id            = aws_vpc.isucon.id
+  cidr_block        = "172.31.32.0/20"
+  availability_zone = "ap-northeast-1d"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "isucon Public d"
+  }
+}
+
+resource "aws_internet_gateway" "isucon_igw" {
+  vpc_id = aws_vpc.isucon.id
+  tags = {
+    Name = "isucon IGW"
+  }
+}
+
+resource "aws_route_table" "isucon_public" {
+  vpc_id = aws_vpc.isucon.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.isucon_igw.id
+  }
+
+  tags = {
+    Name = "isucon Public"
+  }
+}
+
+resource "aws_route_table_association" "isucon_public_a" {
+  subnet_id      = aws_subnet.isucon_public_a.id
+  route_table_id = aws_route_table.isucon_public.id
+}
+
+resource "aws_route_table_association" "isucon_public_c" {
+  subnet_id      = aws_subnet.isucon_public_c.id
+  route_table_id = aws_route_table.isucon_public.id
+}
+
+resource "aws_route_table_association" "isucon_public_d" {
+  subnet_id      = aws_subnet.isucon_public_d.id
+  route_table_id = aws_route_table.isucon_public.id
+}
+
+# NOTE. https://ip-ranges.amazonaws.com/ip-ranges.json で公開されているAWSのCIDRをTerraformから取得する
+data "aws_ip_ranges" "ec2_instance_connect" {
+  regions  = ["ap-northeast-1"]
+  services = ["ec2_instance_connect"]
+}
+
+resource "aws_security_group" "isucon" {
+  vpc_id      = aws_vpc.isucon.id
+  name        = "isucon"
+  description = "isucon"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["3.113.59.168/32", "13.113.172.248/32"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["3.113.59.168/32", "13.113.172.248/32"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["3.113.59.168/32", "13.113.172.248/32"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["172.31.0.0/16"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = data.aws_ip_ranges.ec2_instance_connect.cidr_blocks
+    ipv6_cidr_blocks = data.aws_ip_ranges.ec2_instance_connect.ipv6_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "isucon"
+  }
+}
+
+resource "aws_security_group" "isucon_bench" {
+  vpc_id      = aws_vpc.isucon.id
+  name        = "isucon_bench"
+  description = "isucon_bench"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["3.113.59.168/32", "13.113.172.248/32"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["3.113.59.168/32", "13.113.172.248/32"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["3.113.59.168/32", "13.113.172.248/32"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = data.aws_ip_ranges.ec2_instance_connect.cidr_blocks
+    ipv6_cidr_blocks = data.aws_ip_ranges.ec2_instance_connect.ipv6_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "isucon_bench"
+  }
+}
