ef5c1338ae46f19f5e25e45ddd795074c31ccb86
okazaki-kk <kohei-okazaki@c-fo.com>
2023-10-23 11:22:16 +0900

    :+1: ec2同士の通信するterraformを追加

---
+++ ec2/ec2.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "default" {
+  ami                         = "ami-08a706ba5ea257141"
+  instance_type               = "t2.micro"
+  key_name                    = "otyamura-isucon"
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.default.id]
+
+  tags = {
+    Name = "${local.name}-default"
+  }
+}
+
+resource "aws_instance" "web1" {
+  ami           = "ami-08a706ba5ea257141"
+  instance_type = "t2.micro"
+  key_name      = "otyamura-isucon"
+  subnet_id     = module.vpc.private_subnets[0]
+  vpc_security_group_ids = [aws_security_group.internal.id]
+
+  tags = {
+    Name = "${local.name}-internal"
+  }
+}
---
+++ ec2/security_group.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "default" {
+  name                   = "${local.name}-default"
+  vpc_id                 = module.vpc.vpc_id
+  revoke_rules_on_delete = false
+  description            = "security group for default"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["3.113.59.168/32", "13.113.172.248/32"]
+  }
+}
+
+resource "aws_security_group" "internal" {
+  name                   = "${local.name}-internal"
+  vpc_id                 = module.vpc.vpc_id
+  revoke_rules_on_delete = false
+  description            = "security group for internal"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    security_groups = [aws_security_group.default.id]
+  }
+}
