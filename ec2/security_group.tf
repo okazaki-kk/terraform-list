36de0bf591c1fa1e5c1438c03b1848e8f36f72fe
okazaki-kk <kohei-okazaki@c-fo.com>
2023-10-23 11:22:56 +0900

    terraform fmt

--- ec2/security_group.tf
+++ ec2/security_group.tf
@@ -32,9 +32,9 @@ resource "aws_security_group" "internal" {
   }
 
   ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
     security_groups = [aws_security_group.default.id]
   }
 }
