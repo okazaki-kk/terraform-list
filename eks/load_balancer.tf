f41d7b47a456cca3d703edefc5112d36d6cfe5ea
okazaki-kk <kohei-okazaki@c-fo.com>
2023-12-06 19:34:15 +0900

    fix eks

--- eks/load_balancer.tf
+++ eks/load_balancer.tf
@@ -7,32 +7,12 @@ resource "aws_lb" "alb" {
 
 resource "aws_lb_listener" "alb_listener" {
   load_balancer_arn = aws_lb.alb.arn
-  port              = "80"
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code  = "200"
-    }
-  }
-}
-
-resource "aws_lb_listener_rule" "alb_listener_rule" {
-  listener_arn = aws_lb_listener.alb_listener.arn
-
-  action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.target_group.arn
-  }
-
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
   }
 }
 
@@ -40,8 +20,20 @@ resource "aws_lb_target_group" "target_group" {
   name        = "${local.name}-target-group"
   port        = 30000
   protocol    = "HTTP"
-  target_type = "ip"
+  target_type = "instance"
   vpc_id      = module.vpc.vpc_id
+
+  health_check {
+    protocol            = "HTTP"
+    matcher             = 200
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment_30000" {
+  autoscaling_group_name = module.eks.eks_managed_node_groups_autoscaling_group_names[0]
+  lb_target_group_arn    = aws_lb_target_group.target_group.arn
 }
 
 # ALBのセキュリティグループ
